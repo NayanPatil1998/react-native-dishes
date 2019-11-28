e965de43c8941380949adaf74bc55b8a3342913c
Nayan Patil <patil.nayan04@gmail.com>
2019-11-28 20:31:58 +0530

    Gesture part 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -27,6 +27,8 @@ function RenderDish({
     openCommentForm,
 }) {
 
+    handleViewRef = ref => this.view = ref;
+
     const recognizeDrag =({moveX, moveY, dx, dy}) =>{
         if(dx < -200)
             return true
@@ -34,9 +36,13 @@ function RenderDish({
             return false
     }
 
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true
+        },
+        onPanResponderGrant:() => {
+            this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'))
         },
         onPanResponderEnd:(e, gestureState) => {
             console.log("pan responder end", gestureState);
@@ -57,7 +63,9 @@ function RenderDish({
 
     if (dish != null) {
         return (
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}{...panResponder.panHandlers}>
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+            ref={this.handleViewRef}
+            {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={dish.name}
                     image={{ uri: baseUrl + dish.image }}
